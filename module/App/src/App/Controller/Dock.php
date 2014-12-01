d8f553d04878298e2ad640381c8194c5446ded4d
James McGinnis <jamess99@iastate.edu>
2014-11-30 20:37:13 -0500

    added lockall / unlockall to docks and bikes

--- module/App/src/App/Controller/Dock.php
+++ module/App/src/App/Controller/Dock.php
@@ -20,9 +20,36 @@ class Dock extends AbstractController
     			'bicycle' => [
     				'label' => 'Bicycle',
     			],
+    			'locked' => [
+    				'label' => 'Locked',
+    			],
     		],
     		'query' => $query,
     	]);
         return compact('docks');
     }
+
+
+    public function LockallAction()
+    {
+        $docks = $this->entity('Dock')->findAll();
+
+	foreach($docks as $dock)
+	{
+		$dock->lock();
+	}
+        $this->entity()->flush();
+        return $this->redirect()->toRoute('app/docks');
+    }
+    public function UnlockallAction()
+    {
+        $docks = $this->entity('Dock')->findAll();
+	foreach($docks as $dock)
+	{
+		$dock->unlock();
+	}
+        $this->entity()->flush();
+        return $this->redirect()->toRoute('app/docks');
+    }
+
 }
